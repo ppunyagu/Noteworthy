111ec8f92b286b3256b2bf7a1689d75f5d393b14
Phongpol <phongpol@snapbag.co.th>
2017-01-31 16:43:39 -0500

    BluetoothLEActivity that sends and receive from device

--- Noteworthy/Activity/BluetoothLEActivity.cs
+++ Noteworthy/Activity/BluetoothLEActivity.cs
@@ -20,12 +20,23 @@ namespace Noteworthy
 	[Activity(Label = "BluetoothLEActivity", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class BluetoothLEActivity : Activity
 	{
-		protected async override void OnCreate(Bundle savedInstanceState)
+		public UUID RX_SERVICE_UUID = UUID.FromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+    	public UUID RX_CHAR_UUID = UUID.FromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+    	public UUID TX_CHAR_UUID = UUID.FromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+		public UUID CCCD = UUID.FromString("00002902-0000-1000-8000-00805f9b34fb");
+
+		public Button sendButton;
+
+		public BluetoothGatt mBluetoothGatt;
+
+		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
 			// Create your application here
 			SetContentView(Resource.Layout.MainRecording);
+
+			sendButton = FindViewById<Button>(Resource.Id.btnRecord);
 
 			// Set this on if want Audio Background: 
 			Utility.server_heartRate = "http://157.252.187.36:5000";
@@ -46,38 +57,26 @@ namespace Noteworthy
 				_scanCallBack.OnDeviceFound += (sender, device) =>
 				{
 					adapter.BluetoothLeScanner.StopScan(_scanCallBack);
-					BluetoothGatt mBluetoothGatt;
 					BluetoothLEGattCallback mGattCallback = new BluetoothLEGattCallback();
-					mGattCallback.OnDeviceReady += (sendFunction, gatt) =>
+					mGattCallback.OnDeviceReadyWrite += (sendFunction, gatt) =>
 					{
 						try
 						{
-							byte[] bufferWrite = ASCIIEncoding.Default.GetBytes("hi from up");
-							BluetoothGattService service = gatt.GetService(UUID.FromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e"));
-							if (service == null)
+							byte[] bufferWrite = ASCIIEncoding.Default.GetBytes("master");
+							BluetoothGattService RxService = gatt.GetService(RX_SERVICE_UUID);
+							BluetoothGattCharacteristic RxChar = RxService.GetCharacteristic(RX_CHAR_UUID);
+							RxChar.SetValue(bufferWrite);
+							if (gatt.WriteCharacteristic(RxChar))
 							{
-								Log.Debug("OnDeviceFound", "Service is null");
-								return;
-							}
-							BluetoothGattCharacteristic characteristic = service.GetCharacteristic(UUID.FromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e"));
-							if (characteristic == null)
-							{
-								Log.Debug("OnDeviceFound", "Characteristic is null");
-								return;
-							}
-							characteristic.SetValue(bufferWrite);
-							characteristic.WriteType = GattWriteType.NoResponse;
-							if (gatt.WriteCharacteristic(characteristic))
-							{
-								Log.Debug("OnDeviceFound", "Write Successfull!");
+								Log.Debug("OnDeviceReadyWrite", "Write Successfull!");
 							}
 							else {
-								Log.Debug("OnDeviceFound", "Write Unsuccessful... :(");
+								Log.Debug("OnDeviceReadyWrite", "Write Unsuccessful... :(");
 							}
 						}
 						catch (Exception ex)
 						{
-							Utility.ExceptionHandler("BluetoothLEActivity", "OnDeviceFound", ex);
+							Utility.ExceptionHandler("BluetoothLEActivity", "OnDeviceReadyWrite", ex);
 						}
 					};
 					mBluetoothGatt = device.ConnectGatt(this, true, mGattCallback);
@@ -124,7 +123,6 @@ namespace Noteworthy
 			else {
 				Log.Debug("LEScanCallBack", "No device detected!");
 			}
-			base.OnScanResult(callbackType, result);
 		}
 
 		public override void OnBatchScanResults(IList<ScanResult> results)
@@ -137,15 +135,23 @@ namespace Noteworthy
 					Log.Debug("LEScanCallBack", string.Format("Address: {0}, Rssi: {1}", result.Device, result.Rssi));
 				}
 			}
-			base.OnBatchScanResults(results);
 		}
 	}
 
 	public class BluetoothLEGattCallback : BluetoothGattCallback
 	{
-		public delegate void DeviceReady(object sendFunction, BluetoothGatt gatt);
+		public UUID RX_SERVICE_UUID = UUID.FromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+		public UUID RX_CHAR_UUID = UUID.FromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+		public UUID TX_CHAR_UUID = UUID.FromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+		public UUID CCCD = UUID.FromString("00002902-0000-1000-8000-00805f9b34fb");
 
-		public event DeviceReady OnDeviceReady;
+		public delegate void DeviceReadyWrite(object sendFunction, BluetoothGatt gatt);
+
+		public event DeviceReadyWrite OnDeviceReadyWrite;
+
+		public bool isWrite = false;
+
+		public bool isRead = false;
 
 		public override void OnConnectionStateChange(BluetoothGatt gatt, GattStatus status, ProfileState newState)
 		{
@@ -153,22 +159,61 @@ namespace Noteworthy
 			{
 				gatt.DiscoverServices();
 			}
-			base.OnConnectionStateChange(gatt, status, newState);
 		}
 
 		public override void OnServicesDiscovered(BluetoothGatt gatt, GattStatus status)
 		{
-			var handler = OnDeviceReady;
+			var handler = OnDeviceReadyWrite;
 			if (status == GattStatus.Success)
 			{
-				foreach (var service in gatt.Services)
+				BluetoothGattService RxService = gatt.GetService(RX_SERVICE_UUID);
+				BluetoothGattCharacteristic TxChar = RxService.GetCharacteristic(TX_CHAR_UUID);
+				gatt.SetCharacteristicNotification(TxChar, true);
+
+				BluetoothGattDescriptor descriptor = TxChar.GetDescriptor(CCCD);
+				descriptor.SetValue(BluetoothGattDescriptor.EnableNotificationValue.ToArray());
+				gatt.WriteDescriptor(descriptor);
+			}
+		}
+
+		public override void OnDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, GattStatus status)
+		{
+			if (status == GattStatus.Success)
+			{
+				var handler = OnDeviceReadyWrite;
+				if (!isWrite)
 				{
-					foreach (var characteristic in service.Characteristics)
-					{
-						Log.Debug("OnServicesDiscovered", string.Format("Service @{0}\nCharacteristics @{1}\n Permission: {2}", service.Uuid, characteristic.Uuid, characteristic.Properties));
-					}
+					Log.Debug("OnServicesDiscovered", "Is now ready to write!");
+					handler(this, gatt);
+					isWrite = true;
 				}
-				handler(this, gatt);
+			}
+		}
+
+		public override void OnCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic)
+		{
+			byte[] value = characteristic.GetValue();
+			var result = ASCIIEncoding.Default.GetString(value);
+			Log.Debug("BluetoothLEGattCallback", string.Format("Result: {0}", result));
+		}
+
+		public override void OnCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, GattStatus status)
+		{
+			try
+			{
+				if (status == GattStatus.Success)
+				{
+					Log.Debug("OnCharacteristicRead", "Is read successfully!");
+					byte[] value = characteristic.GetValue();
+					Log.Debug("BluetoothLEGattCallback", string.Format("String Value: {0}", characteristic.GetStringValue(0)));
+					var result = System.Text.Encoding.UTF8.GetString(value);
+					Log.Debug("BluetoothLEGattCallback", string.Format("Result: {0}", result));
+				}
+				base.OnCharacteristicRead(gatt, characteristic, status);
+			}
+			catch (Exception ex)
+			{
+				Utility.ExceptionHandler("BluetoothLEGattCallback", "OnCharacteristicRead", ex);
 			}
 		}
 	}
